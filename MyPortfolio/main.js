4c9ec323ca9dd353a1e1884101d53fa669cf3416
KeshavM <k.mishra04@outlook.com>
2024-10-07 19:48:48 +0530

    Installed vite bundler, transferred previous files to the new folder

--- MyPortfolio/main.js
+++ MyPortfolio/main.js
@@ -1,38 +1,38 @@
 //This is for toggling the "light-theme" class on/off
 document.querySelector("#toggle-theme").addEventListener("click", () => {
-    document.documentElement.classList.toggle("light-theme");
+  document.documentElement.classList.toggle("light-theme");
 });
 
 const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
 
 if (prefersLight) {
-    document.documentElement.classList.add("light-theme");
+  document.documentElement.classList.add("light-theme");
 }
 
 //This is for the arrow in the intro section
 document.querySelector("#scroll-down").addEventListener("click", () => {
-    window.scrollTo({
-        top: document.querySelector("#about-me").offsetTop - 20,
-    });
+  window.scrollTo({
+      top: document.querySelector("#about-me").offsetTop - 20,
+  });
 });
 
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("carousel_text")
+  let i;
+  let slides = document.getElementsByClassName("carousel_text")
 
-    if (n > slides.length) {slideIndex = slides.length}
-    if (n < 1) {slideIndex = 1}
+  if (n > slides.length) {slideIndex = slides.length}
+  if (n < 1) {slideIndex = 1}
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("active")
-    }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active")
+  }
 
-    slides[slideIndex - 1].classList.add("active");
+  slides[slideIndex - 1].classList.add("active");
 }
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlides(slideIndex += n);
 }
 
 let slideIndex = 1;
@@ -40,10 +40,10 @@ showSlides(slideIndex);
 
 const previous = document.querySelector("#previous_btn");
 previous.addEventListener("click", () => {
-    plusSlides(-1);
+  plusSlides(-1);
 })
 
 const next = document.querySelector("#next_btn");
 next.addEventListener("click", () => {
-    plusSlides(1);
+  plusSlides(1);
 })
